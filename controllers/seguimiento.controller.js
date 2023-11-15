8e774f25ebe9a0733fd208b4440842f2153aa833
Tomas Steven Desktop <tomssteven22@gmail.com>
2023-11-15 11:36:04 -0300

    find clients v1

--- controllers/seguimiento.controller.js
+++ controllers/seguimiento.controller.js
@@ -5,7 +5,7 @@ const findClient = async (req, res) => {
 
   try {
     const clients = await Client.find({ dni: dni });
-    res.status(200).json(clients[0].estadoPedido)
+    res.status(200).json({estado : clients[0].estadoPedido, cliente:  clients[0].nombre, fecha: clients[0].fechaCompra, pedido : clients[0].producto})
   } catch (error) {
     res.status(404).json({ message: "No se encontro a ninguna persona con ese DNI." });
   }
