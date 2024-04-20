75166e0a12b78db91c8fddfec9436bfc6e363589
Tomas Steven Desktop <tomssteven22@gmail.com>
2024-04-20 16:04:30 -0300

    segundo pedido en seguimiento  fixed

--- controllers/seguimiento.controller.js
+++ controllers/seguimiento.controller.js
@@ -5,14 +5,19 @@ const findClient = async (req, res) => {
 
   try {
     const clients = await Client.find({ dni: dni, eliminado: false });
-    console.log(clients);
     /* res.status(200).json({estado : clients[0].estadoPedido, cliente:  clients[0].nombre, fecha: clients[0].fechaCompra, pedido : clients[0].producto, link: clients[0].linkSeguimiento}) */
-    res.status(200).json(clients);
+    clients.length > 0
+      ? res.status(200).json(clients)
+      : res
+          .status(404)
+          .json({ message: "No se encontro a ninguna persona con ese DNI." });
   } catch (error) {
-    res.status(404).json({ message: "No se encontro a ninguna persona con ese DNI." });
+    res
+      .status(404)
+      .json({ message: "Error en el Servidor." });
   }
 };
 
 module.exports = {
-  findClient
+  findClient,
 };
